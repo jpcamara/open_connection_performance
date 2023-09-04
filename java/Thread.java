909d22a3d01abb4073e129dd0be1a8d31d73f1fe
JP Camara <48120+jpcamara@users.noreply.github.com>
2023-09-03 21:50:05 -0400

    VirtualThread example in java
    * Modify the Dockerfile to run the basic thread example, which is about 30% slower (19 seconds vs 27 seconds)

---
+++ java/Thread.java
@@ -0,0 +1,46 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+int NUM_THREADS = 8000;
+String ENDPOINT_URL = "http://host.docker.internal:8080/echo";
+
+void main() throws InterruptedException {
+  System.out.println(NUM_THREADS + " threads will be created");
+  System.out.println(ENDPOINT_URL + " will be called");
+  HttpClient client = HttpClient.newHttpClient();
+
+  Runnable runnable = () -> {
+    HttpRequest request = HttpRequest.newBuilder()
+      .uri(URI.create(ENDPOINT_URL))
+      .GET()
+      .build();
+
+    try {
+      client.send(request, HttpResponse.BodyHandlers.ofString());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  };
+
+  long startTime = System.currentTimeMillis();
+  List<Thread> virtualThreads = new ArrayList<>();
+  for (int i = 0; i < NUM_THREADS; i++) {
+    Thread t = new Thread(runnable);
+    t.start();
+    virtualThreads.add(t);
+  }
+
+  for (Thread virtualThread : virtualThreads) {
+    virtualThread.join();
+  }
+
+  long endTime = System.currentTimeMillis();
+  long elapsedTime = endTime - startTime;
+
+  System.out.println("All virtual threads completed.");
+  System.out.println("Elapsed time: " + elapsedTime + " milliseconds.");
+}
--- readme.md
+++ readme.md
@@ -5,3 +5,10 @@ cd echo_server
 docker-compose up --build -d # first time
 docker-compose up            # after building
 ```
+
+### Java
+
+```
+docker build -t java-virtual-thread .
+docker run -it --rm --name run-those-threads java-virtual-thread
+```
